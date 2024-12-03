629deb670f9f0850eaaa0beac2ea00bb73b84fcf
carlosmuller <kdu428@gmail.com>
2024-12-03 19:14:12 -0300

    chore(db): add idx em user.cpf

--- src/main/java/br/com/fiap/totem_express/infrastructure/user/UserEntity.java
+++ src/main/java/br/com/fiap/totem_express/infrastructure/user/UserEntity.java
@@ -4,19 +4,19 @@ import java.time.LocalDateTime;
 
 import br.com.fiap.totem_express.domain.user.Role;
 import br.com.fiap.totem_express.domain.user.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
 import static jakarta.persistence.EnumType.STRING;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 
 @Entity(name = "user")
+@Table(indexes = {
+        @Index(columnList = "cpf", name="idx_user_cpf")
+})
 public class UserEntity {
 
     @Id
